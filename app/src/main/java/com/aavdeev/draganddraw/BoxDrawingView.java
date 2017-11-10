736e52c72a00c1c1f66cb0c7f445bb6d5cd8e9ff
avdey <avdey87@gmail.com>
2017-11-10 13:45:47 +0300

    564 Отслеживание перемещений между событиями

--- app/src/main/java/com/aavdeev/draganddraw/BoxDrawingView.java
+++ app/src/main/java/com/aavdeev/draganddraw/BoxDrawingView.java
@@ -1,13 +1,19 @@
 package com.aavdeev.draganddraw;
 
 import android.content.Context;
+import android.graphics.PointF;
+import android.nfc.Tag;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 import java.util.jar.Attributes;
 
 public class BoxDrawingView extends View {
-    //используется при создании представления в коде
+    private static final String TAG = "BoxDrawingView";
+
+       //используется при создании представления в коде
     public BoxDrawingView(Context context) {
         this(context, null);
     }
@@ -15,5 +21,27 @@ public class BoxDrawingView extends View {
     //используется при заполнении представления по разметки XML
     public BoxDrawingView(Context context, AttributeSet attrs) {
         super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        PointF current = new PointF(event.getX(), event.getY());
+        String action = "";
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                action = "ACTION_DOWN";
+                break;
+            case MotionEvent.ACTION_MOVE:
+                action = "ACTION_MOVE";
+                break;
+            case MotionEvent.ACTION_UP:
+                action = "ACTION_UP";
+            case MotionEvent.ACTION_CANCEL:
+                action = "ACTION_CANCEL";
+                break;
+        }
+        Log.i(TAG, action + "at x=" + current.x + ", y=" + current.y);
+        return true;
     }
 }
